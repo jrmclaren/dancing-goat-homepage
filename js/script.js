a4c5ff8770386819d1bfd5baf20c78e8cf5fe71d
Jason McLaren <17043524+jrmclaren@users.noreply.github.com>
2018-04-30 09:58:48 +1000

    added functionality so menu will close on selction

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 document.addEventListener('DOMContentLoaded', function () {
     var menu = document.getElementById('menu');
     var navBg = document.getElementsByClassName('nav-bg')[0];
@@ -7,21 +9,34 @@ document.addEventListener('DOMContentLoaded', function () {
     var menuContainer = document.querySelector('.menu-container');
     var toggleActive = function toggleActive() {
         if (menuContainer.classList.contains('active')) {
+            alert('unmounting menu');
             menuContainer.classList.remove('active');
             navBg.classList.remove('active');
             nav.classList.remove('active');
         } else {
+            alert('mounting menu');
             menuContainer.classList.add('active');
             navBg.classList.add('active');
             nav.classList.add('active');
         }
     };
     menu.addEventListener('change', toggleActive);
+
+    // If a selction is made the menu
+    // should also toggle
+    var handleSelection = function handleSelection() {
+        toggleActive();
+    };
+    // add handleSelection listener to allMenuLinks
+    var allMenuLinks = [].concat(_toConsumableArray(document.querySelectorAll('.nav__link')));
+    allMenuLinks.map(function (link) {
+        link.addEventListener('click', handleSelection);
+    });
 });
 
 /*
 UTILITY FUNCTIONS
-
+WIP
  */
 
 var removeClasses = function removeClasses(className, elements) {
@@ -36,6 +51,5 @@ var removeClasses = function removeClasses(className, elements) {
             alert('Exit');
     }
 };
-removeClasses('active', 'someid');
 
 //# sourceMappingURL=script.js.map
--- js/script.precomp.js
+++ js/script.precomp.js
@@ -1,28 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-const menu = document.getElementById('menu');
-const navBg = document.getElementsByClassName('nav-bg')[0];
-const nav = document.getElementsByClassName('nav')[0];
-const menuContainer = document.querySelector('.menu-container');
-const toggleActive = () => {
-    if( menuContainer.classList.contains('active') ){
-        menuContainer.classList.remove('active');
-        navBg.classList.remove('active');
-        nav.classList.remove('active');
-    } else {
-        menuContainer.classList.add('active');
-        navBg.classList.add('active');
-        nav.classList.add('active');
-    }
-};
-menu.addEventListener('change', toggleActive);
-});
+    const menu = document.getElementById('menu');
+    const navBg = document.getElementsByClassName('nav-bg')[0];
+    const nav = document.getElementsByClassName('nav')[0];
+    const menuContainer = document.querySelector('.menu-container');
+    const toggleActive = () => {
+        if( menuContainer.classList.contains('active') ){
+            alert('unmounting menu');
+            menuContainer.classList.remove('active');
+            navBg.classList.remove('active');
+            nav.classList.remove('active');
+        } else {
+            alert('mounting menu');
+            menuContainer.classList.add('active');
+            navBg.classList.add('active');
+            nav.classList.add('active');
+        }
+    };
+    menu.addEventListener('change', toggleActive);
 
+    // If a selction is made the menu
+    // should also toggle
+    const handleSelection = () => {
+        toggleActive();
+        };
+    // add handleSelection listener to allMenuLinks
+    const allMenuLinks = [...document.querySelectorAll('.nav__link')];
+    allMenuLinks.map( link => {
+        link.addEventListener( 'click', handleSelection);
+    });
+});
 
 
 
 /*
 UTILITY FUNCTIONS
-
+WIP
  */
 
 const removeClasses = ( className, elements ) => {
@@ -37,4 +49,3 @@ const removeClasses = ( className, elements ) => {
             alert('Exit');
     }
 };
-removeClasses('active', 'someid');
