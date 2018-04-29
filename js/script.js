6dc29224f88812cd601d0c5a3c2edca3544c3adf
Jason McLaren <17043524+jrmclaren@users.noreply.github.com>
2018-04-29 17:19:31 +1000

    Introduced burger menu system. TODO: squash menu bugz from codepen dump

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-"use strict";
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var menu = document.getElementById('menu');
+    var navBg = document.getElementsByClassName('nav-bg')[0];
+    var nav = document.getElementsByClassName('nav')[0];
+    var menuContainer = document.querySelector('.menu-container');
+    var toggleActive = function toggleActive() {
+        if (menuContainer.classList.contains('active')) {
+            menuContainer.classList.remove('active');
+            navBg.classList.remove('active');
+            nav.classList.remove('active');
+        } else {
+            menuContainer.classList.add('active');
+            navBg.classList.add('active');
+            nav.classList.add('active');
+        }
+    };
+    menu.addEventListener('change', toggleActive);
+});
 
 //# sourceMappingURL=script.js.map
--- js/script.precomp.js
+++ js/script.precomp.js
@@ -1,4 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+const menu = document.getElementById('menu');
+const navBg = document.getElementsByClassName('nav-bg')[0];
+const nav = document.getElementsByClassName('nav')[0];
+const menuContainer = document.querySelector('.menu-container');
+const toggleActive = () => {
+    if( menuContainer.classList.contains('active') ){
+        menuContainer.classList.remove('active');
+        navBg.classList.remove('active');
+        nav.classList.remove('active');
+    } else {
+        menuContainer.classList.add('active');
+        navBg.classList.add('active');
+        nav.classList.add('active');
+    }
+};
+menu.addEventListener('change', toggleActive);
 
-
-
+});
